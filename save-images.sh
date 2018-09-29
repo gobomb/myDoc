fc02590df12c0d84b96812586a0ee9dd203dec52
gobomb <bombcoder@outlook.com>
2018-09-29 17:38:25 +0800

    Create save-images.sh

---
+++ save-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+i=0
+curl -L https://raw.githubusercontent.com/gobomb/myDoc/master/k8s-image | while read image
+do
+        tarname=$(echo $image|sed "s#/#_#g")
+        docker save $image >$PWD/saved-images/$tarname.tar
+        echo $i: $tarname.tar saved
+        ((i++))
+done
+
+tar zcvf saved-images.tar.gz saved-images
